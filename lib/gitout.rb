f9d3811cf631e579b5c3e607a3a6f09d1f608954
Jonathan Torres <jonathantorres41@gmail.com>
2015-06-21 00:29:03 +0000

    Use literals.

--- lib/gitout.rb
+++ lib/gitout.rb
@@ -2,9 +2,9 @@ class Gitout
 
   def github(username)
     data = {
-      :commits => 100,
-      :additions => 50,
-      :deletions => 30,
+      commits: 100,
+      additions: 50,
+      deletions: 30,
     }
 
     data
