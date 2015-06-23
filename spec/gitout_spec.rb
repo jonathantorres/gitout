709fdd086c7c8a09445c785d60f5b750cade4e7a
Jonathan Torres <jonathantorres41@gmail.com>
2015-06-23 02:51:40 +0000

    Set expectations.

--- spec/gitout_spec.rb
+++ spec/gitout_spec.rb
@@ -2,6 +2,15 @@ require 'gitout'
 
 RSpec.describe Gitout do
   it "fetches user data from github" do
+    github = double('github')
+    repo = double('repo')
+    commit = double('commit')
+    commits = [commit, commit]
+
+    allow(github).to receive(:repositories).once.with('jonathantorres').and_return([repo, repo])
+    allow(github).to receive(:commits).twice.and_return(commits, commits)
+    allow(github).to receive(:commit).exactly(4).times.and_return(commit, commit, commit, commit)
+
     data = Gitout.new.github('jonathantorres')
 
     expect(data[:commits]).to eq(100)
