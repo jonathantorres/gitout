fbdbc20a1d7dbe7002a84f42659a87faa769dc6e
Jonathan Torres <jonathantorres41@gmail.com>
2015-06-24 02:25:18 +0000

    Fix tests.

--- spec/gitout_spec.rb
+++ spec/gitout_spec.rb
@@ -3,18 +3,19 @@ require 'gitout'
 RSpec.describe Gitout do
   it "fetches user data from github" do
     github = double('github')
-    repo = double('repo')
-    commit = double('commit')
+    repo = double('repo', :name => 'some-repo')
+    commit = double('commit', :sha => 'SOMESHA', :stats => { additions: 10, deletions: 20 })
     commits = [commit, commit]
 
-    allow(github).to receive(:repositories).once.with('jonathantorres').and_return([repo, repo])
-    allow(github).to receive(:commits).twice.and_return(commits, commits)
-    allow(github).to receive(:commit).exactly(4).times.and_return(commit, commit, commit, commit)
+    expect(github).to receive(:repositories).once.with('jonathantorres').and_return([repo, repo])
+    expect(github).to receive(:commits).twice.and_return(commits, commits)
+    expect(github).to receive(:commit).exactly(4).times.and_return(commit, commit, commit, commit)
 
-    data = Gitout.new.github('jonathantorres')
+    gitout = Gitout.new(github, 'jonathantorres')
+    data = gitout.github
 
-    expect(data[:commits]).to eq(100)
-    expect(data[:additions]).to eq(50)
-    expect(data[:deletions]).to eq(30)
+    expect(data[:commits]).to eq(4)
+    expect(data[:additions]).to eq(40)
+    expect(data[:deletions]).to eq(80)
   end
 end
